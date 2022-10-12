1538b3885e917710fe2bb9fbedc2c5e4112805d2
Morgován Nimród <morgovannimrod22@gmail.com>
2022-10-12 14:28:13 +0200

    error message a felhasználónévhez

--- src/index.js
+++ src/index.js
@@ -2,16 +2,34 @@
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('username').addEventListener('input', () => {
         let user = document.getElementById('username');
-        let chars = /[/&=_'+,<>-]/;
+        let chars = /[/&=_'-+,<>]/;
+        let error = document.getElementById('errorUserName');
         if (chars.test(user.value)) {
             user.value = user.value.substring(0, user.value.length - 1);
+            error.textContent = "Nem adhat meg ilyen karaktert!";
+            error.style.padding = '5 px';
+            setTimeout(() => {
+                error.textContent = "";
+            }, 1000);
+            error.style.padding = '0 px';
         }
-        if(user.value.length - 1 === 0 && user.value == "."){
+        if (user.value.length - 1 === 0 && user.value == ".") {
             user.value = user.value.substring(0, user.value.length - 1);
+            error.textContent = "Nem kezdhet ponttal!";
+            error.style.padding = '5 px';
+            setTimeout(() => {
+                error.textContent = "";
+            }, 1000);
+            error.style.padding = '0 px';
         }
-        if(user.value.substring(user.value.length-2, user.value.length-1) == "."){
+        if (user.value.substring(user.value.length - 2, user.value.length - 1) == ".") {
+            error.textContent = "Nem rakhat egymás után 2 pontot!";
+            error.style.padding = '5 px';
+            setTimeout(() => {
+                error.textContent = "";
+            }, 1000);
+            error.style.padding = '0 px';
             user.value = user.value.substring(0, user.value.length - 1);
         }
     });
-    console.log("loaded")
 });
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,35 @@
+
 document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('username')!.addEventListener('input', () => {
         let user = document.getElementById('username') as HTMLInputElement;
         let chars = /[/&=_'-+,<>]/;
+        let error =document.getElementById('errorUserName') as HTMLLabelElement;
         if (chars.test(user.value)) {
-            user.value = user.value.substring(0, user.value.length-1)
+            user.value = user.value.substring(0, user.value.length-1);
+            error.textContent = "Nem adhat meg ilyen karaktert!";
+            error.style.padding = '5 px';
+            setTimeout(() => {
+                error.textContent = "";
+            }, 1000);
+            error.style.padding = '0 px';
+        }
+        if(user.value.length - 1 === 0 && user.value == "."){
+            user.value = user.value.substring(0, user.value.length - 1);
+            error.textContent = "Nem kezdhet ponttal!";
+            error.style.padding = '5 px';
+            setTimeout(() => {
+                error.textContent = "";
+            }, 1000);
+            error.style.padding = '0 px';
+        }
+        if(user.value.substring(user.value.length-2, user.value.length-1) == "."){
+            error.textContent = "Nem rakhat egymás után 2 pontot!";
+            error.style.padding = '5 px';
+            setTimeout(() => {
+                error.textContent = "";
+            }, 1000);
+            error.style.padding = '0 px';
+            user.value = user.value.substring(0, user.value.length - 1);
         }
     });
 });
