9d40322cdc69c5f40c35605e75c9696cb8e2aa8b
Morgován Nimród <morgovannimrod22@gmail.com>
2022-10-12 12:55:26 +0200

    átírás folytatása

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 "use strict";
+document.addEventListener('DOMContentLoaded', () => {
+    var _a;
+    (_a = document.getElementById('username')) === null || _a === void 0 ? void 0 : _a.addEventListener('change', () => {
+    });
+});
